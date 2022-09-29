20c5ff44d34339a73c4772743740f56b4cc926dd
Bilal Malik <malikzaman133@gmail.com>
2022-09-29 18:17:39 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import {
+  createStore,
+  compose,
+  applyMiddleware,
+  bindActionCreators,
+  combineReducers
+} from "redux";
+
+const reducer = (state) => state;
+const monitorEnhancer = (createStore) => (reducer, initialState, enhancer) => {
+  const monitoredReducer = (state, action) => {
+    const start = performance.now();
+    const newState = reducer(state, action);
+    const end = performance.now();
+
+    const diff = end - start;
+    console.log(diff);
+    return newState;
+  };
+  return createStore(monitoredReducer, initialState, enhancer);
+};
+
+const store = createStore(reducer, monitorEnhancer);
